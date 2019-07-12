b6958295f9ed249a5f654c26e13cbf9c1a8c513d
ukronholm <ukronholm@parasoft.com>
2019-07-12 16:15:40 -0700

    Checkpoint: sucessfull stub generation for random Linux source files.

--- usr/include/sys/types.h
+++ usr/include/sys/types.h
@@ -15,17 +15,18 @@
 typedef signed __SIZE_TYPE__ ssize_t;
 #undef unsigned
 
+#include <linux/types.h>
 #include <linux/posix_types.h>
 #include <asm/types.h>
 
 /* Keeps linux/types.h from getting included elsewhere */
-#define _LINUX_TYPES_H
+//#define _LINUX_TYPES_H
 
 typedef __kernel_fd_set fd_set;
 typedef uint32_t dev_t;
 typedef __kernel_ino_t ino_t;
 typedef __kernel_mode_t mode_t;
-typedef __kernel_loff_t off_t;
+//typedef __kernel_loff_t off_t;
 typedef __kernel_loff_t loff_t;
 typedef __kernel_pid_t pid_t;
 typedef __kernel_daddr_t daddr_t;
